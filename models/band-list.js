af4f030e87c0088f27b296d2bb18be97f605af3a
Daniel Mateu <danielmateu86@gmail.com>
2023-05-09 13:00:24 +0200

    Modelos para manejar las bandas

--- models/band-list.js
+++ models/band-list.js
@@ -1,6 +1,7 @@
 const Band = require("./band");
 
 class BandList {
+    
     constructor() {
         this.bands = [
             new Band('Queen'),
@@ -9,4 +10,38 @@ class BandList {
             new Band('Metallica'),
         ]
     }
+
+    addBand(name) {
+        const newBand = new Band(name);
+        this.bands.push(newBand);
+        return this.bands;
+    }
+
+    removeBand(id) {
+        this.bands = this.bands.filter(band => band.id !== id);
+    }
+
+    getBands() {
+        return this.bands;
+    }
+
+    increaseVotes(id) {
+        this.bands = this.bands.map(band => {
+            if (band.id === id) {
+                band.votes += 1;
+            }
+            return band;
+        });
+    }
+
+    changeName(id, newName) {
+        this.bands = this.bands.map(band => {
+            if (band.id === id) {
+                band.name = newName;
+            }
+            return band;
+        });
+    }
 }
+
+module.exports = BandList;
